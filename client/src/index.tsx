eee4af673bf84055c9872e48d20de0fbc9ebf136
marcfsr <faussurier.marc@icloud.com>
2019-05-30 08:54:44 +0200

    Loader and better gitignore

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,33 @@
 import "./../scss/test.scss";
+import $ from "jquery";
+import { read } from "fs";
 
-console.log(`You're all set! 23`);
+declare var window: any;
 
+function addCssLink(url: string) {
+    var cssId = window.btoa(url); 
+    if (!document.getElementById(cssId))
+    {
+        var head  = document.getElementsByTagName('head')[0];
+        var link  = document.createElement('link');
+        link.id   = cssId;
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = url;
+        link.media = 'all';
+        head.appendChild(link);
+    }
+}
 
-window.document.addEventListener("loadeddata", function() { console.log("helo");});
+function onDeviceReady() {
+    console.log("CORDOVA LOADED ... WAITING FOR JQUERY ... ");
+    $( document ).ready(() => { 
+        console.log("JQUERY OK ... ");
+        addCssLink('http://127.0.0.1:5000/dist/main.css');
+
+        $("jsl > div#app-container > div#content-container > div#omnibox-container").append("<div class=\"cde_loader\"></div>")
+    });
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
+window["$"] = $;
